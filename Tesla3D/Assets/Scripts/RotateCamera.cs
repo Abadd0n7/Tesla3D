4deb65ea88e61cb9c740b608c14c080201071210
Abadd0n <lpytlak27@gmail.com>
2019-08-06 01:16:25 +0200

    App version 2.0.0.

--- Tesla3D/Assets/Scripts/RotateCamera.cs
+++ Tesla3D/Assets/Scripts/RotateCamera.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    Camera mainCamera;
     public Transform target;//the target object
     private readonly float speedMod = 5.0f;//a speed modifier
     private Vector3 point;//the coord to the point where the camera looks at
 
+    float touchesPrevPositionDiff, touchesCurrPositionDiff, zoomMod;
+    Vector2 firstTouchPrevPosition, secondTouchPrevPosition;
+
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
     void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
@@ -17,6 +25,34 @@ public class RotateCamera : MonoBehaviour
             transform.RotateAround(target.position, Vector3.down, -touchDeltaPosition.x * speedMod * Time.deltaTime);
             transform.RotateAround(target.position, Vector3.right, -touchDeltaPosition.y * speedMod * Time.deltaTime * 0.5f);
         }
+        
+        if (Input.touchCount == 2)
+        {
+            Touch firstTouch, secondTouch;
+            firstTouch = Input.GetTouch(0);
+            secondTouch = Input.GetTouch(1);
+
+            firstTouchPrevPosition = firstTouch.position - firstTouch.deltaPosition;
+            secondTouchPrevPosition = secondTouch.position - secondTouch.deltaPosition;
+
+            touchesPrevPositionDiff = (firstTouchPrevPosition - secondTouchPrevPosition).magnitude;
+            touchesCurrPositionDiff = (firstTouch.position - secondTouch.position).magnitude;
+
+            zoomMod = touchesPrevPositionDiff - touchesCurrPositionDiff;
+
+            //if(touchesPrevPositionDiff > touchesCurrPositionDiff)
+            //{
+            //    mainCamera.fieldOfView += zoomMod * Time.deltaTime;
+            //}
+            //if (touchesPrevPositionDiff < touchesCurrPositionDiff)
+            //{
+            //    mainCamera.fieldOfView -= zoomMod * Time.deltaTime;
+            //}
+            mainCamera.fieldOfView += zoomMod * 0.1f;
+
+        }
+        mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, 60f, 150f);
+
     }
 
 }
